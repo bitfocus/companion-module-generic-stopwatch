0102a5104ae16fe6b71819a3385c050b722da642
Anne Stellingwerf <anne.stellingwerf@gmail.com>
2024-06-09 14:57:23 +0200

    Fix and test #4 and #5

---
+++ src/__tests__/variables.spec.js
@@ -0,0 +1,39 @@
+const variables = require("../variables");
+
+describe("variables", () => {
+  it("rounds correctly", () => {
+    checkVariableValues({ watch: 1500 }, "0", "00", "01", "500");
+  });
+
+  it("pads correctly", () => {
+    checkVariableValues({ watch: 42 }, "0", "00", "00", "042");
+  });
+});
+
+function checkVariableValues(initial, hours, minutes, seconds, milliseconds) {
+  let variableValues = runCheckVariables(initial);
+
+  expect(variableValues.hours).toBe(hours);
+  expect(variableValues.minutes).toBe(minutes);
+  expect(variableValues.seconds).toBe(seconds);
+  expect(variableValues.milliseconds).toBe(milliseconds);
+  expect(variableValues.hmsms).toBe(
+    `${hours}:${minutes}:${seconds}.${milliseconds}`
+  );
+}
+
+function runCheckVariables(initial) {
+  let variableValues = null;
+
+  Object.assign(
+    {
+      ...initial,
+      setVariableValues: (v) => {
+        variableValues = v;
+      },
+    },
+    { ...variables }
+  ).checkVariables();
+  
+  return variableValues;
+}
